b1e0ac1075466b932874c630c0fa206f3fa42d7b
inksne <debanyj23@gmail.com>
2024-12-04 09:10:52 +0300

    add tests with db

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,36 +1,39 @@
 import pytest
-import os
 from typing import AsyncGenerator
-from dotenv import load_dotenv
 from sqlalchemy.future import select
-from database.database import get_async_session, Base, engine, create_db_and_tables
 from database.models import User
+from database.database import async_session_maker
+from .test_config import MODE
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
-load_dotenv('.test.env')
-MODE = os.getenv('MODE')
 print(MODE)
 
 
-@pytest.fixture(scope="function")
-async def setup_test_db() -> AsyncGenerator[AsyncSession, None]:
-    """Фикстура для создания базы данных и предоставления сессии для тестов."""
+@pytest.fixture
+async def setup_test_db(scope="function") -> AsyncGenerator[AsyncSession, None]:
+    '''фикстура для создания базы данных и предоставления сессии для тестов'''
     assert MODE == 'TEST'
-    await create_db_and_tables() 
-
-    async with get_async_session() as session:
+    async with async_session_maker() as session:
         try:
-            yield session  
+            yield session
         finally:
-            session.rollback()  
-            async with engine.begin() as conn:
-                await conn.run_sync(Base.metadata.drop_all)
+            await session.rollback()
+            await session.close()
+
+
+@pytest.mark.asyncio
+async def test_get_something(setup_test_db):
+    assert MODE == 'TEST'
+    async for session in setup_test_db: 
+        result = await session.execute(select(User).where(User.id == 2))
+        data = result.scalars().one_or_none()
+        assert data == None
 
 
 @pytest.mark.asyncio
 async def test_model_type(setup_test_db):
-    """тест для проверки типа возвращаемого объекта из базы данных"""
+    '''тест для проверки типа возвращаемого объекта из базы данных'''
     assert MODE == 'TEST'
     async for session in setup_test_db:  
         result = await session.execute(select(User))
@@ -41,7 +44,7 @@ async def test_model_type(setup_test_db):
 
 @pytest.mark.asyncio
 async def test_create_user(setup_test_db):
-    """тест для проверки создания пользователя в базе данных"""
+    '''тест для проверки создания пользователя в базе данных'''
     assert MODE == 'TEST'
     async for session in setup_test_db:  
         new_user = User(username="testuser", email="testuser@example.com", password="password")
@@ -57,7 +60,7 @@ async def test_create_user(setup_test_db):
 
 @pytest.mark.asyncio
 async def test_delete_user(setup_test_db):
-    """тест для проверки создания пользователя в базе данных"""
+    '''тест для проверки создания пользователя в базе данных'''
     assert MODE == 'TEST'
     async for session in setup_test_db:  
         new_user = User(username="testuser", email="testuser@example.com", password="password")
@@ -73,3 +76,9 @@ async def test_delete_user(setup_test_db):
         user = result.scalars().all()
 
         assert len(user) == 0
+
+
+@pytest.mark.asyncio
+async def test_dummy():
+    '''проверка, что тесты вообще работаюют'''
+    assert True
