df8fcc7ab53de18a0fe47f44026f18fd557bc32c
inksne <debanyj23@gmail.com>
2024-12-10 20:31:36 +0300

    change models Message and UserChat (json and relationships)

--- database/models.py
+++ database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String, TIMESTAMP, Column, Boolean, ForeignKey, ARRAY
+from sqlalchemy import Integer, String, TIMESTAMP, Column, Boolean, ForeignKey, JSON
 from sqlalchemy.sql import func
 from sqlalchemy.orm import DeclarativeBase, relationship
 
@@ -8,42 +8,31 @@ class Base(DeclarativeBase):
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    username = Column(String(length=24), nullable=False)
+    username = Column(String(length=24), unique=True, nullable=False)
     email = Column(String)
     password = Column(String(length=1024), nullable=False)
     registered_at = Column(TIMESTAMP, server_default=func.now())
-    active = Column(Boolean, default = True, nullable=False)
+    active = Column(Boolean, default=True, nullable=False)
 
-
-    sent_messages = relationship('Message', foreign_keys='Message.sender_id', back_populates='sender')
-    received_messages = relationship('Message', foreign_keys='Message.receiver_id', back_populates='receiver')
-
+    sent_messages = relationship('Message', back_populates='author')
 
 
 class Message(Base):
     __tablename__ = 'messages'
     id = Column(Integer, primary_key=True)
-    sender_id = Column(Integer, ForeignKey(User.id), nullable=False)
-    receiver_id = Column(Integer, ForeignKey(User.id), nullable=False)
+    author_username = Column(String(length=24), ForeignKey('users.username'), nullable=False)
     content = Column(String(length=512), nullable=False)
     sended_at = Column(TIMESTAMP, server_default=func.now())
+    chat_id = Column(Integer, ForeignKey('user_chats.id'), nullable=False)
 
-
-    sender = relationship('User', foreign_keys=[sender_id], back_populates='sent_messages')
-    receiver = relationship('User', foreign_keys=[receiver_id], back_populates='received_messages')
-    chat = relationship(
-        'UserChat', 
-          foreign_keys='UserChat.last_message_id', 
-            back_populates='last_message'
-    )
-
+    author = relationship('User', back_populates='sent_messages')
+    chat = relationship('UserChat', back_populates='messages', foreign_keys=[chat_id])  
 
 
 class UserChat(Base):
     __tablename__ = 'user_chats'
     id = Column(Integer, primary_key=True)
-    participants = Column(ARRAY(String), nullable=False)
-    last_message_id = Column(Integer, ForeignKey(Message.id))
+    participants = Column(JSON)
     last_message_time = Column(TIMESTAMP, server_default=func.now())
-    
-    last_message = relationship('Message', foreign_keys=[last_message_id], back_populates='chat')
+
+    messages = relationship('Message', back_populates='chat', lazy='dynamic')  
