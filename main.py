a3271238d7d4dcf6464b97e0550dcef7092e0238
inksne <debanyj23@gmail.com>
2024-12-30 19:55:06 +0300

    change on_event to lifespan

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from fastapi import Depends, FastAPI, Form, APIRouter, HTTPException
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from starlette import status
+from contextlib import asynccontextmanager
 
 from database.models import User
 from database.database import create_db_and_tables, get_async_session
 from auth.utils import hash_password
 from auth.auth import router as jwt_router
 from templates.router import router as base_router
+from config import PORT
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
@@ -15,7 +17,9 @@ from sqlalchemy.future import select
 from typing import Optional
 from pydantic import BaseModel
 from datetime import datetime
-from datetime import datetime
+
+
+PORT = int(PORT)
 
 
 app = FastAPI(title='messenger')
@@ -42,9 +46,12 @@ class UserResponse(BaseModel):
 
 #бд
 
-@app.on_event("startup")
-async def startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     await create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.post('/register', response_model=UserResponse)
