36d115bdeef15226da0979363e0dee7eb0746f36
inksne <debanyj23@gmail.com>
2024-12-31 16:53:13 +0300

    del db_port and db_host

--- database/database.py
+++ database/database.py
@@ -2,10 +2,10 @@ from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
-from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+from config import DB_NAME, DB_PASS, DB_USER
 from .models import Base
 
-DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@localhost:5432/{DB_NAME}'
 
 
 engine = create_async_engine(DATABASE_URL)
