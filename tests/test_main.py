d34f23f81eece8155a1ef17e763feebc96690faa
inksne <debanyj23@gmail.com>
2024-12-31 16:59:28 +0300

    del useless test

--- tests/test_main.py
+++ tests/test_main.py
@@ -24,23 +24,3 @@ def test_read_register_page():
 def test_read_login_page():
     response = client.get('/jwt/login')
     assert response.status_code == 200
-
-
-def test_login():
-    username = "inksne"
-    password = "ink"
-    response = client.post(
-        "/jwt/login/",
-        data={"username": username, "password": password}
-    )
-    assert response.status_code == 200
-
-    cookies = response.cookies
-    access_token = cookies.get("access_token")
-    refresh_token = cookies.get("refresh_token")
-
-    assert access_token is not None
-    assert refresh_token is not None
-
-    assert len(access_token) > 0
-    assert len(refresh_token) > 0
